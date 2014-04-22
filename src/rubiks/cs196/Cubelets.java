dab5a00a2a85ede5ae773cb522edb7e1976b0690
acjense2 <jensen.aj@outlook.com>
2014-04-22 01:52:58 -0500

    small edits

--- src/rubiks/cs196/Cubelets.java
+++ src/rubiks/cs196/Cubelets.java
@@ -19,6 +19,10 @@ public class Cubelets {
 			new SimpleVector(0, -i, 0), new SimpleVector(0, -i, i),
 			new SimpleVector(0, 0, i) };
 	public static int number = coordinates.length;
+	
+	//indexes of center cubies g, r, b, o, w, y
+	private static int[] centerCubies = {26,10,22,1,24,20};
+	private static int[][] faceCubies = { {} };
 
 	public static SimpleVector getCoords(int i) {
 		return coordinates[i];
